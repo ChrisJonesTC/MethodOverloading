1f5437ed4b4102d7cce92003fffcccb3794628d9
ChrisJonesTC <cfjones91@gmail.com>
2022-09-19 13:19:31 -0400

    initial commit

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static void Add(int num1, int num2)
+        {
+            Console.WriteLine (num1 + num2);
+        }
+
+        public static void Add(double num1, double num2)
+        {
+            Console.WriteLine (num1 + num2);
+        }
+
+        public static void Add(int num1, int num2, bool isTrue)
+        {
+            var sum = 0;
+            var response = "";
+
+            if(isTrue)
+            {
+             sum = num1 + num2; 
+
+                if(sum == 1)
+                {
+                    response = $"{sum} dollar.";
+                }
+                else
+                {
+                    response = $"{sum} dollars.";
+                }
+
+            } 
+
+            Console.WriteLine (response);
+        }
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,14 @@
+﻿using MethodOverloading;
+using System.ComponentModel;
+
+Methods.Add(4, 6);
+//Console.WriteLine();
+
+Methods.Add(5.7, 7.5);
+//Console.WriteLine();
+
+Methods.Add(5,7);
+//Console.WriteLine();
+
+Methods.Add(1001, 1002, true);
+//Console.WriteLine();
